82b6e2e2a3250cb5611f6d1ac4cb9be5bc7c4c51
Gang Torchinsky <tor4911@yandex.ru>
2021-05-09 14:51:29 +0300

    Скрипты для меню в мобильном режиме

--- js/script.js
+++ js/script.js
@@ -24,3 +24,20 @@ topBtn.onclick = function(e) {
   e.preventDefault();
   scrollToTop();
 }
+
+const btnToggle = document.querySelector('.main-nav__btn-toggle');
+const navMenu = document.querySelector('.main-nav-wrapper');
+
+btnToggle.addEventListener('click', function () {
+  if (btnToggle.classList.contains('main-nav__btn-toggle--burger')) {
+    btnToggle.classList.remove('main-nav__btn-toggle--burger');
+    btnToggle.classList.add('main-nav__btn-toggle--cross');
+    navMenu.classList.remove('main-nav-wrapper--closed');
+    navMenu.classList.add('main-nav-wrapper--opened');
+  } else {
+    btnToggle.classList.remove('main-nav__btn-toggle--cross');
+    btnToggle.classList.add('main-nav__btn-toggle--burger');
+    navMenu.classList.remove('main-nav-wrapper--opened');
+    navMenu.classList.add('main-nav-wrapper--closed');
+}
+})
